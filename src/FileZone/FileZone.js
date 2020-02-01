28a3372b2d587c8d5a7721d98518e55f45146020
Andrew Ratnikov <ratnikov.am@gmail.com>
2020-02-01 22:13:56 +0200

    Modify project structure

---
+++ src/FileZone/FileZone.js
@@ -0,0 +1,17 @@
+// Core
+import React from 'react';
+
+// Styles
+import './FileZone.css';
+
+const FileZone = () => {
+    return (
+        <div id="file-zone">
+            <div id="file">
+
+            </div>
+        </div>
+    );
+}
+
+export default FileZone;
